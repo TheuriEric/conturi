8200eabf32275ae0a511bbde6f0d6f89b912150e
Eric Theuri <tericmail001@gmail.com>
2025-10-17 00:06:41 +0300

    Added router logic

--- backend/src/conturi/app.py
+++ backend/src/conturi/app.py
@@ -1,7 +1,3 @@
-from langchain_groq  import ChatGroq
-from langchain.prompts import ChatPromptTemplate
-from langchain_core.output_parsers import StrOutputParser
-import os
 from dotenv import load_dotenv
 from .components import logger, router
 
